8a96f91c6f35c1c208f908a6e0dc1eedabc75c6d
Wacoder Forever <sethomondi088@gmail.com>
2024-08-04 18:51:21 +0300

    ghhg

--- datedetect.py
+++ datedetect.py
@@ -1,11 +1,11 @@
 import re
 
 def leap_year(year):
-    if (year%4==0):
-        if(year%100!=0):
+    if (year % 4 == 0):
+        if (year % 100 != 0):
             return True
         else:
-            if(year%400==0):
+            if (year % 400 == 0):
                 return True
             else:
                 return False
@@ -13,20 +13,23 @@ def leap_year(year):
         return False
 
 def date_regex(date):
-    dateRegex=re.compile(r'^[0-3][0-9]\/[0-1][0-2]\/[1-2][0-9][0-9][0-9]$')
-    mo=re.findall(string)(date)
-    k=mo.group()
-    return k
+    dateRegex = re.compile(r'^[0-3][0-9]/[0-1][0-2]/[1-2][0-9]{3}$')
+    mo = dateRegex.search(date)
+    if mo:
+        return mo.group()
+    else:
+        return None
 
 def main():
-    day=input("Enter day:")
-    month=input("Enter month:")
-    year=input("Enter year:")
-    date=day+'/'+month+'/'+year
-    result=date_regex(date)
-    if result is not NULL:
-        print(date+" is valid")
+    day = input("Enter day: ")
+    month = input("Enter month: ")
+    year = input("Enter year: ")
+    date = f"{day}/{month}/{year}"
+    result = date_regex(date)
+    if result:
+        print(f"{date} is valid")
     else:
-        print(date+" is not valid")
-if __name__=='__main__':
+        print(f"{date} is not valid")
+
+if __name__ == '__main__':
     main()
