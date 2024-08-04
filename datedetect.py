f26a19bbda24621250a1f598004eae3ec9c90eae
Wacoder Forever <sethomondi088@gmail.com>
2024-08-04 17:00:55 +0300

    gfgfdf

--- datedetect.py
+++ datedetect.py
@@ -1,6 +1,18 @@
 import re
 
 def leap_year(year):
+    if (year%4==0):
+        if(year%100!=0):
+            return True
+        else:
+            if(year%400==0):
+                return True
+            else:
+                return False
+    else:
+        return False
+
+def valid_date():
     
 
 if __name__=='__main__':
