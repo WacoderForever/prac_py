d0d0f0271e51bf83762d5b4e9e3134931f20c796
Wacoder Forever <sethomondi088@gmail.com>
2024-08-05 05:33:43 +0300

    shdfkjdfs

--- whatsappmsg.py
+++ whatsappmsg.py
@@ -3,8 +3,7 @@ import pyautogui
 import time
 
 phonenumber="+254720507316"
-message="Oyaaah morio hii text imetumwa na Python"
+message="Pia hii imetumwa na Python"
 pywhatkit.sendwhatmsg_instantly(phonenumber,message)
-
 time.sleep(30)
 pyautogui.press("enter")
