cdb29fdfb0d3b0aa285bbf4b57490a67a50e3762
WacoderForever <sethomondi088@gmail.com>
2026-02-10 14:49:59 +0300

    multi segementation

--- machine_learning/chap1/multi_seg.py
+++ machine_learning/chap1/multi_seg.py
@@ -14,4 +14,20 @@ points = df.iloc[:, 1:5].values
 kmeans = KMeans(n_clusters=6,random_state=0)
 kmeans.fit(points)
 df['Cluster'] = kmeans.predict(points)
-print(df.head())
+
+results = pd.DataFrame(columns=['Cluster', 'Average Age', 'Average Income(k$)',
+                                'Average Spending Index(0-100)', 'Number of Females',
+                                'Number of Males'])
+
+for i,center in enumerate(kmeans.cluster_centers_):
+    age = center[1]         #Average age for current cluster
+    income = center[2]      #Average income for current cluster 
+    spend = center[3]       #Average spending score for current cluster
+
+    gdf = df[df['Cluster']==i]
+    females = gdf[gdf['Gender']==0].shape[0]
+    males = gdf[gdf['Gender']==1].shape[0]
+
+    results.loc[i] = ([i,age,income,spend,females,males])
+
+print(results)
