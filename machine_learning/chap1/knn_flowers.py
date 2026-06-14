18376c4bf10a85b0d8fd80084f577b5f347e29df
WacoderForever <sethomondi088@gmail.com>
2026-06-15 00:56:06 +0300

    knn

--- machine_learning/chap1/knn_flowers.py
+++ machine_learning/chap1/knn_flowers.py
@@ -6,13 +6,14 @@ from sklearn.neighbors import KNeighborsClassifier
 iris = load_iris()
 df = pd.DataFrame(iris.data,columns=iris.feature_names)
 df['class'] = iris.target
-df['class_name'] = iris.target_names[iris['target']]
-# Train test split
+df['class name'] = iris.target_names[iris.target]
+
+# splitting training and test data
 x_train,x_test,y_train,y_test = train_test_split(iris.data,iris.target,test_size=0.2,random_state=0)
-# Train ML model
+
+# training the model
 model = KNeighborsClassifier(n_neighbors=10)
 model.fit(x_train,y_train)
-# Test model accuracy
-print(model.score(x_test,y_test))
 
+print(model.score(x_test,y_test))
 print(model.predict([[5.6, 4.4, 1.2, 0.4]]))
