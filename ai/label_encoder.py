7e6471ce11bd83709678d5198854342ed8a2f115
WacoderForever <sethomondi088@gmail.com>
2026-07-10 02:08:22 +0300

    label encoding

---
+++ ai/label_encoder.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sklearn import preprocessing
+
+input_labels = ['red', 'black', 'red', 'green', 'black', 'yellow', 'white']
+
+# Create label encoder and fit the labels
+encoder = preprocessing.LabelEncoder()
+encoder.fit(input_labels)
+
+# Print the mapping
+print("\nLabel mapping:")
+for i, item in enumerate(encoder.classes_):
+    print(item, '-->', i)
+
+# Encode values using the encoder
+test_labels = ['green', 'red', 'black']
+encoded_values = encoder.transform(test_labels)
+print("\nLabels =", test_labels)
+print("Encoded values =", list(encoded_values))
+
+# Decode values using the encoder
+encoded_values = [3, 0, 4, 1]
+decoded_list = encoder.inverse_transform(encoded_values)
+print("\nEncoded values: ", encoded_values)
+print("Decoded list: ", decoded_list)
